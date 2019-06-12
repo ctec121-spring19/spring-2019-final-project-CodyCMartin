b9dfa24664b9d6d10a88f37c5ccfd35c8833f744
codycmartin <codycmartin89@gmail.com>
2019-06-11 22:45:02 -0700

    converted coord in view to 3,3 to calculate cells better

--- src/View.py
+++ src/View.py
@@ -9,23 +9,24 @@ from graphics import *
 class View:
 
     def __init__(self):
-        # Window creation
+        # Window creation/coords
         self._win = GraphWin("Tic Tac Toe", 600,600)
+        self._win.setCoords(0, 0, 3, 3)
 
         # Grid line creation
-        Line(Point(200,50),Point(200,550)).draw(self._win)
-        Line(Point(400,50),Point(400,550)).draw(self._win)
-        Line(Point(50,200),Point(550,200)).draw(self._win)
-        Line(Point(50,400),Point(550,400)).draw(self._win)
+        Line(Point(1,.25),Point(1,2.75)).draw(self._win)
+        Line(Point(2,.25),Point(2,2.75)).draw(self._win)
+        Line(Point(.25,1),Point(2.75,1)).draw(self._win)
+        Line(Point(.25,2),Point(2.75,2)).draw(self._win)
 
         # Top text "tic tac toe"
-        self.topText = Text(Point(300,25),"Tic Tac Toe")
+        self.topText = Text(Point(1.5,2.92),"Tic Tac Toe")
         self.topText.setStyle("bold")
         self.topText.setSize(18)
         self.topText.draw(self._win)
 
         # Bottom text formatting and location 
-        self.bottomText = Text(Point(300,575),"")
+        self.bottomText = Text(Point(1.5,.12),"")
         self.bottomText.setStyle("bold")
         self.bottomText.setSize(18)
         self.bottomText.draw(self._win)
@@ -34,7 +35,7 @@ class View:
     # function to return coords when clicked
     def getClick(self):
         point = self._win.getMouse()        
-        return(point.getX(), point.getY())
+        return int(point.getX()), int(point.getY())
 
     # this text shows before the start of each game 
     def startText(self, message):
