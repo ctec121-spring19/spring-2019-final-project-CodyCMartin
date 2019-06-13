bb5a608cf358452ec456a2a9629e2134a35d82c7
codycmartin <codycmartin89@gmail.com>
2019-06-12 20:12:09 -0700

    Fixed if statements in model

--- src/Model.py
+++ src/Model.py
@@ -24,40 +24,40 @@ class Model:
             return True
         elif cell == "cell1" and board[0] != "":
             return False
-        if cell == "cell2" and board[2] == "":
+        elif cell == "cell2" and board[2] == "":
             return True
         elif cell == "cell2" and board[2] != "":
             return False
-        if cell == "cell3" and board[3] == "":
+        elif cell == "cell3" and board[3] == "":
             return True
         elif cell == "cell3" and board[3] != "":
             return False
-        if cell == "cell4" and board[4] == "":
+        elif cell == "cell4" and board[4] == "":
             return True
         elif cell == "cell4" and board[4] != "":
             return False
-        if cell == "cell5" and board[5] == "":
+        elif cell == "cell5" and board[5] == "":
             return True
         elif cell == "cell5" and board[5] != "":
             return False
-        if cell == "cell6" and board[6] == "":
+        elif cell == "cell6" and board[6] == "":
             return True
         elif cell == "cell6" and board[6] != "":
             return False
-        if cell == "cell7" and board[7] == "":
+        elif cell == "cell7" and board[7] == "":
             return True
         elif cell == "cell7" and board[7] != "":
             return False
-        if cell == "cell8" and board[8] == "":
+        elif cell == "cell8" and board[8] == "":
             return True
         elif cell == "cell8" and board[8] != "":
             return False
-        if cell == "cell9" and board[9] == "":
+        elif cell == "cell9" and board[9] == "":
             return True
         elif cell == "cell9" and board[9] != "":
             return False
     
-
+# test
 def ModelTest():
     m = Model()
     m.cellSelection("cell1")
